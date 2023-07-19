2de2106b73380d6131c8a0267d160415a7be069a
Samarth Dambalkar <samarthdambalkar7@gmail.com>
2023-07-19 16:39:40 +0530

    initial commit

---
+++ src/test/java/com/photoconfigserver/photoconfigserver/PhotoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoconfigserver.photoconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
