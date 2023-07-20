21702c61ab5b790738ed27b9e7f867eca73941c7
Samarth Dambalkar <samarthdambalkar7@gmail.com>
2023-07-20 16:13:25 +0530

    added config to single file and changed default label

--- src/main/java/com/photoconfigserver/photoconfigserver/PhotoConfigServerApplication.java
+++ src/main/java/com/photoconfigserver/photoconfigserver/PhotoConfigServerApplication.java
@@ -11,5 +11,5 @@ public class PhotoConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PhotoConfigServerApplication.class, args);
     }
-
+        
 }
